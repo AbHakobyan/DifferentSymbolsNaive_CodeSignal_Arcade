809d2807af280ea06170573198a55112512c06f5
Albert Hakobyan <abhakobyan24@gmail.com>
2021-09-27 23:53:09 +0400

    Добавьте файлы проекта.

---
+++ DifferentSymbolsNaive_CodeSignal_Arcade/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DifferentSymbolsNaive_CodeSignal_Arcade
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string st = "asadaa";
+            int x = differentSymbolsNaive(st);
+            Console.WriteLine(x);
+        }
+
+        static int differentSymbolsNaive(string s)
+        {
+            HashSet<char> list = new HashSet<char>();
+            for (int i = 0; i < s.Length; i++)
+            {
+                list.Add(s[i]);
+            }
+            return list.Count;
+        }
+
+    }
+}
